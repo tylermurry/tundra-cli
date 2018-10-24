95c8b05a74c6377d5dd35ab17bbaad9f875a3d1a
Aditya Koukuntla <Aditya.Koukuntla@jbhunt.com>
2018-10-24 14:54:49 -0500

    Added a method to create a file with methods to get profile data

--- src/services/profile.js
+++ src/services/profile.js
@@ -1,8 +1,11 @@
 import mkdirp from 'async-mkdirp';
 import fs from 'fs';
+import path from 'path';
 import { getState } from './state';
 
-export const PROFILE_DIRECTORY = () => `${process.cwd()}/${getState().fixturesDirectory}/profiles`;
+const PROFILES = 'profiles';
+const FIXTURES_DIRECTORY = () => `${process.cwd()}/${getState().fixturesDirectory}`;
+export const PROFILE_DIRECTORY = () => `${FIXTURES_DIRECTORY()}/${PROFILES}`;
 
 export const getExistingProfileRequests = async(profile) => {
   try {
@@ -15,4 +18,24 @@ export const getExistingProfileRequests = async(profile) => {
 export const saveRequestsAsProfile = async function(requests, profile) {
   await mkdirp(PROFILE_DIRECTORY());
   await fs.writeFileSync(`${PROFILE_DIRECTORY()}/${profile}.json`, JSON.stringify(requests, null, 2), 'utf8');
+  await createProfileData();
+};
+
+const createProfileData = async function() {
+    const profileDataFilePath = `${FIXTURES_DIRECTORY()}/profileData.js`;
+    try {
+        await fs.unlinkSync(`${profileDataFilePath}`);
+        console.log('successfully deleted '+profileDataFilePath);
+    } catch (e) {
+        console.log('There is no profile data yet!');
+    }
+
+    let profileDataContent = '';
+    const files = await fs.readdirSync(PROFILE_DIRECTORY());
+    files.forEach((file) => {
+       const profileName = path.basename(file, '.json');
+       profileDataContent += `export const ${profileName.toLowerCase()} = () => require('./${PROFILES}/${profileName}');\n\n`;
+    });
+
+    await fs.writeFileSync(profileDataFilePath, profileDataContent);
 };
